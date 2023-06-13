966df3f07a3b5967ae495e6e4261ac9626ad11cc
sfrick <s.frick@reply.de>
2023-06-13 23:36:30 +0200

    remove some packages

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo dnf install git stow curl gcc -y
 
 sh <(curl -L https://nixos.org/nix/install) --no-daemon
 
-. /home/sfrick/.nix-profile/etc/profile.d/nix.sh
+\. /home/sfrick/.nix-profile/etc/profile.d/nix.sh
 
 NIXPKGS_ALLOW_UNFREE=1 nix-env -iA \
 	nixpkgs.neovim \
@@ -17,13 +17,10 @@ NIXPKGS_ALLOW_UNFREE=1 nix-env -iA \
 	nixpkgs.rofi \
 	nixpkgs.waybar \
 	nixpkgs.neofetch \
-	nixpkgs.fnm \
 	nixpkgs.python311 \
 	nixpkgs.python311Packages.pip \
-	nixpkgs.nvm \
 	nixpkgs.rustup
 
-fnm install 16
 rustup default stable
 
 stow *
