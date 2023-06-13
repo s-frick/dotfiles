f83f99efd57ccd24e526eb04627fc0bb0b9a111f
sfrick <s.frick@reply.de>
2023-06-13 22:26:14 +0200

    add packages installer

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+sudo dnf install git stow curl
+
+sh <(curl -L https://nixos.org/nix/install) --no-daemon
+
+. /home/sfrick/.nix-profile/etc/profile.d/nix.sh
+
+NIXPKGS_ALLOW_UNFREE=1 nix-env -iA \
+	nixpkgs.google-chrome \
+	nixpkgs.spotify \
+	nixpkgs.tmux \
+	nixpkgs.tmuxinator \
+	nixpkgs.grim \ 
+nixpkgs.wlogout \ 
+nixpkgs.rofi \ 
+nixpkgs.waybar \ 
+nixpkgs.neofetch \ 
+nixpkgs.fnm \ 
+nixpkgs.python311 \ 
+nixpkgs.python311Packages.pip
+nixpkgs.nvm \ 
+nixpkgs.rustup
+
+fnm install 16
+rustup default stable
